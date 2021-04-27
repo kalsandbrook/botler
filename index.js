0d32ad262f2503ad034b900bc3568ada34d6ee53
v-briese <valentine.briese.07@gmail.com>
2021-04-27 11:45:05 -0700

    Add more console logs

--- index.js
+++ index.js
@@ -6,87 +6,90 @@ const { app_id, guild_id } = require("./config.json");
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-const globalCommandFiles = fs
-	.readdirSync("./commands/global")
-	.filter((file) => file.endsWith(".js"));
-
-let globalCommandsRegistered = 0;
-
-for (const file of globalCommandFiles) {
-	const command = require(`./commands/global/${file}`);
-
-	client.api
-		.applications(app_id)
-		.commands.post({ data: command.command })
-		.then(() => {
-			client.commands.set(command.command.name, command);
-
-			globalCommandsRegistered += 1;
-
-			if (globalCommandsRegistered >= globalCommandFiles.length)
-				client.api
-					.applications(app_id)
-					.commands.get()
-					.then((response) => {
-						for (const command of response) {
-							if (
-								!globalCommandFiles.find(
-									(file) => file.slice(0, file.length - 3) == command.name
-								)
-							)
-								client.api.applications(app_id).commands(command.id).delete();
-						}
-					});
-		});
-}
-
-const guildCommandFiles = fs
-	.readdirSync("./commands/guild")
-	.filter((file) => file.endsWith(".js"));
-
-let guildCommandsRegistered = 0;
-
-for (const file of guildCommandFiles) {
-	const command = require(`./commands/guild/${file}`);
-
-	client.api
-		.applications(app_id)
-		.guilds(guild_id)
-		.commands.post({ data: command.command })
-		.then(() => {
-			client.commands.set(command.command.name, command);
-
-			guildCommandsRegistered += 1;
-
-			if (guildCommandsRegistered >= guildCommandFiles.length)
-				client.api
-					.applications(app_id)
-					.guilds(guild_id)
-					.commands.get()
-					.then((response) => {
-						for (const command of response) {
-							if (
-								!guildCommandFiles.find(
-									(file) => file.slice(0, file.length - 3) == command.name
-								)
-							)
-								client.api
-									.applications(app_id)
-									.guilds(guild_id)
-									.commands(command.id)
-									.delete();
-						}
-					});
-		});
-}
-
 client.once("ready", () => {
+	const globalCommandFiles = fs
+		.readdirSync("./commands/global")
+		.filter((file) => file.endsWith(".js"));
+
+	let globalCommandsRegistered = 0;
+
+	for (const file of globalCommandFiles) {
+		const command = require(`./commands/global/${file}`);
+
+		client.api
+			.applications(app_id)
+			.commands.post({ data: command.command })
+			.then(() => {
+				client.commands.set(command.command.name, command);
+
+				globalCommandsRegistered += 1;
+
+				if (globalCommandsRegistered >= globalCommandFiles.length)
+					client.api
+						.applications(app_id)
+						.commands.get()
+						.then((response) => {
+							for (const command of response) {
+								if (
+									!globalCommandFiles.find(
+										(file) => file.slice(0, file.length - 3) == command.name
+									)
+								)
+									client.api.applications(app_id).commands(command.id).delete();
+							}
+						});
+			});
+	}
+	console.log("[GLOBAL COMMANDS REGISTERED]");
+
+	const guildCommandFiles = fs
+		.readdirSync("./commands/guild")
+		.filter((file) => file.endsWith(".js"));
+
+	let guildCommandsRegistered = 0;
+
+	for (const file of guildCommandFiles) {
+		const command = require(`./commands/guild/${file}`);
+
+		client.api
+			.applications(app_id)
+			.guilds(guild_id)
+			.commands.post({ data: command.command })
+			.then(() => {
+				client.commands.set(command.command.name, command);
+
+				guildCommandsRegistered += 1;
+
+				if (guildCommandsRegistered >= guildCommandFiles.length)
+					client.api
+						.applications(app_id)
+						.guilds(guild_id)
+						.commands.get()
+						.then((response) => {
+							for (const command of response) {
+								if (
+									!guildCommandFiles.find(
+										(file) => file.slice(0, file.length - 3) == command.name
+									)
+								)
+									client.api
+										.applications(app_id)
+										.guilds(guild_id)
+										.commands(command.id)
+										.delete();
+							}
+						});
+			});
+	}
+	console.log("[GUILD COMMANDS REGISTERED]");
+
 	client.user.setActivity({
 		type: "PLAYING",
 		name: `at v${require("./package.json").version}`,
 	});
+	console.log("[ACTIVITY SET]");
 
-	console.log("Ready!");
+	console.log("[READY]");
 });
 
 client.ws.on("INTERACTION_CREATE", async (interaction) => {
