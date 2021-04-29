296cd5b1f67431db163af8b4886906a2ebb57806
v-briese <valentine.briese.07@gmail.com>
2021-04-29 09:36:37 -0700

    I don't really know what's wrong but this kind of fixes it?

--- commands/global/kill.js
+++ commands/global/kill.js
@@ -15,12 +15,13 @@ module.exports = {
 	response: (interaction) => {
 		const victim = interaction.data.options[0].value;
 		const responses = [
+			`Prepare to die, <@${victim}>!`,
 			`Say goodbye, <@${victim}>!`,
 			`Hasta la vista, <@${victim}>!`,
 			`Say hello to my little friend, <@${victim}>!`,
 			`My name is ~~Inigo Montoya~~ Botler. You killed my ~~father~~ creator. Prepare to die, <@${victim}>.`,
 			`I have come here to chew bubblegum and kick ass. And I'm all out of bubblegum, <@${victim}>.`,
-			`I have come here to chew ass and kick bubblegum. And I'm all out of ass, <@${victim}>`,
+			`I have come here to chew ass and kick bubblegum. And I'm all out of ass, <@${victim}>.`,
 			`Long live the king, <@${victim}>!`,
 			`Get off my plane, <@${victim}>.`,
 			`Dodge this, <@${victim}>!`,
@@ -37,7 +38,8 @@ module.exports = {
 					{
 						title: "/kill",
 						description:
-							responses[Math.floor(Math.random() * responses.length - 1)],
+							responses[Math.floor(Math.random() * responses.length)] ||
+							responses[0],
 						color: require("discord.js").Constants.Colors.RED,
 						image: {
 							url:
