d7126205915932bdc4192f49a53f29c5d8ebf347
v-briese <valentine.briese.07@gmail.com>
2021-04-29 09:17:43 -0700

    Fill kill with popular quotes

--- commands/global/kill.js
+++ commands/global/kill.js
@@ -13,13 +13,31 @@ module.exports = {
 	},
 
 	response: (interaction) => {
+		const victim = interaction.data.options[0].value;
+		const responses = [
+			`Say goodbye, <@${victim}>!`,
+			`Hasta la vista, <@${victim}>!`,
+			`Say hello to my little friend, <@${victim}>!`,
+			`My name is ~~Inigo Montoya~~ Botler. You killed my ~~father~~ creator. Prepare to die, <@${victim}>.`,
+			`I have come here to chew bubblegum and kick ass. And I'm all out of bubblegum, <@${victim}>.`,
+			`I have come here to chew ass and kick bubblegum. And I'm all out of ass, <@${victim}>`,
+			`Long live the king, <@${victim}>!`,
+			`Get off my plane, <@${victim}>.`,
+			`Dodge this, <@${victim}>!`,
+			`You shall not pass, <@${victim}>!`,
+			`This is sparta, <@${victim}>!`,
+			`I'm gonna make this pencil disappear, <@${victim}>!`,
+			`You once were a VE-GON, but now you will BE GONE, <@${victim}>!`,
+		];
+
 		return {
 			type: 4,
 			data: {
 				embeds: [
 					{
 						title: "/kill",
-						description: `Say goodbye, <@${interaction.data.options[0].value}>`,
+						description:
+							responses[Math.floor(Math.random() * responses.length - 1)],
 						color: require("discord.js").Constants.Colors.RED,
 						image: {
 							url:
